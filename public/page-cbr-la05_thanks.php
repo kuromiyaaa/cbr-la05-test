0c0d37cbf59145648b6c227bf40f1cb3ac183db8
“Reina <reina.kuromiya@cast-er.com>
2026-01-11 20:04:24 +0900

    up

---
+++ public/page-cbr-la05_thanks.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width,user-scalable=no,shrink-to-fit=yes" />
+	<title>送信完了 | CASTER BIZ recruiting</title>
+
+	<?php wp_head(); ?>
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap"
+		rel="stylesheet">
+	<!-- CSS -->
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/cbr-ai02/base/base.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/cbr-ai02/base/reset.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/cbr-ai02/object/component.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/cbr-ai02/object/style.css">
+
+	<!-- ファビコン -->
+	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico">
+
+</head>
+
+<body class="page">
+
+	<div class="cb">
+
+		<header id="cb-header" class="cb-header">
+			<div class="cb-header__inner">
+				<h1 class="cb-header__logo">
+					<a href="https://recruiting.cast-er.com/cbr-ai02">
+					  <img src="assets/img/logo.png" class="" alt="CASTER BIZ accounting">
+					</a>
+				</h1>
+			</div>
+		</header>
+
+		<main class="cb-main cb-main-thanks">
+
+			<section class="cb-thanks">
+				<div class="cb-thanks__inner">
+					<h2>
+						<span>THANKS</span>
+						<strong>【お問い合わせ】<br class="sp">ありがとうございます</strong>
+					</h2>
+					<div class="form_ly">
+
+						<!-- <div class="form_l">
+							<figure>
+								<img src="<?= get_template_directory_uri() ?>/img/lp1/catalog.png" alt="">
+							</figure>
+						</div> -->
+						<div class="form_r">
+							<p class="explain">
+								お問い合わせいただきありがとうございました<br>
+								担当者よりご連絡をさせて頂きますので今しばらくお待ちくださいませ。<br>
+				連絡がない場合はメールアドレスの入力漏れ、入力間違いなどの可能性がございますので<br>改めてご連絡いただけますと幸いです。
+							</p>
+							<!-- <div class="btn_download">
+								<a href="https://8058161.fs1.hubspotusercontent-na1.net/hubfs/8058161/02_CB%E7%B5%8C%E7%90%86/%E2%98%85CASTER%20BIZ%20%E7%B5%8C%E7%90%86_%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9%E8%B3%87%E6%96%99.pdf"
+									class="btn_style03">
+									<img src="assets/img/cta_orange.png" class="" alt="無料で資料をダウンロード">
+								</a>
+							</div> -->
+						</div>
+					</div>
+		            <div class="cb-thanks__chui">
+                        ※同業者様のお問い合わせはご遠慮いただいており、ご返信できかねる場合がございます。
+				    </div>
+					<!-- <div class="cb-thanks__cta-wrap">
+						<div class="cb-thanks__cta">
+					    <a href="https://recruiting.cast-er.com/case/"><div class="cb-thanks__cta-txt">事例を見る</div></a>
+                        </div>
+						<div class="cb-thanks__cta">
+					    <a href="https://recruiting.cast-er.com/column/"><div class="cb-thanks__cta-txt">コラムを見る</div></a>
+                        </div>
+				    </div> -->
+				</div>
+
+			</section>
+
+
+		</main>
+
+		<footer class="c-footer">
+			<div class="c-footer__inner">
+				<div class="c-footer__link-wrap">
+					<div class="c-footer__link">
+						<a href="https://caster.co.jp/about/" class="c-footer__link-item" target="_blank"
+							id="analy-footer_link01">会社情報</a>
+						<a href="https://caster.co.jp/privacypolicy/" class="c-footer__link-item" target="_blank"
+							id="analy-footer_link02">プライバシーポリシー</a>
+						<a href="https://cast-er.com/legal/" class="c-footer__link-item" target="_blank"
+							id="analy-footer_link03">特定商取引法</a>
+					</div>
+				</div>
+				<div class="c-footer__copyright">
+					Copyright©Caster Co.Ltd. All rights reserved.
+				</div>
+			</div>
+		</footer>
+
+	</div>
+
+
+	<!-- Start of HubSpot Embed Code -->
+	<script id="hs-script-loader" src="//js.hs-scripts.com/8058161.js"></script>
+	<!-- End of HubSpot Embed Code -->
+</body>
+
+</html>
